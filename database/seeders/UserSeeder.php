6ffb124d463f4e3cca9b2debeec76e3145d84604
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-02-24 08:45:26 +0800

    update again

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Helpers\RoleUser;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,6 +15,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'admin',
+            'role'=>RoleUser::ADMIN,
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'is_aktif'=>true
+        ]);
         User::factory()->count(5)->create();
     }
 }
