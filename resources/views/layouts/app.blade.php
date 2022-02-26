908cf7928e60ccc9226bc5db9668954b890a3475
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-02-27 04:37:10 +0800

    layout fix

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kasir</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    @if (!Request::routeIs('auth.*'))
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @endif
+    @livewireStyles
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+    </style>
+</head>
+
+<body>
+    @if (Request::routeIs('auth.*'))
+        {{ $slot }}
+    @else
+        @include('layouts.header')
+        <div class="container-fluid">
+            <div class="row">
+                @include('layouts.sidebar')
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
+    @endif
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @livewireScripts
+</body>
+
+</html>
