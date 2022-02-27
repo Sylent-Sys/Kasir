6759e21c405ea921bb2175a09b9b264b1221a15e
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-02-27 23:36:15 +0800

    add route dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('dashboard') }}">
                     <i class="bi bi-house"></i>
                     Dashboard
                 </a>
