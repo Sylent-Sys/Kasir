195569c211c0f76f856cb6503785c9c49c590cb1
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-02-27 19:50:45 +0800

    pembagian role beres

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Helpers\RoleUser;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -24,7 +26,21 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Gate::define('admin', function(User $user) {
+            return $user->role === RoleUser::ADMIN;
+        });
+        Gate::define('pengguna', function(User $user) {
+            return $user->role === RoleUser::PENGGUNA;
+        });
+        Gate::define('waiter', function(User $user) {
+            return $user->role === RoleUser::WAITER;
+        });
+        Gate::define('kasir', function(User $user) {
+            return $user->role === RoleUser::KASIR;
+        });
+        Gate::define('owner', function(User $user) {
+            return $user->role === RoleUser::OWNER;
+        });
         //
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,11 @@
 <div>
-    <h1>Dashboard</h1>
+    <div class="pt-3 pb-2 mb-3 border-bottom">
+        <h1>Dashboard</h1>
+    </div>
+    @can('admin')
+        @include('livewire.dashboard.admin')
+    @endcan
+    @canany(['pengguna','waiter','kasir','owner'])
+        @include('livewire.dashboard.guest')
+    @endcanany
 </div>
