e1646cc4a39237fc273b995813d052e80d38fe98
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 01:33:34 +0800

    add method to delete old image

--- app/Http/Livewire/Menu/IndexMenu.php
+++ app/Http/Livewire/Menu/IndexMenu.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Menu;
 
 use App\Models\Menu;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class IndexMenu extends Component
@@ -12,6 +13,7 @@ class IndexMenu extends Component
         return view('livewire.menu.index-menu',['data'=>Menu::all()]);
     }
     public function delete(Menu $menu) {
+        Storage::delete($menu->gambar);
         $menu->delete();
     }
 }
