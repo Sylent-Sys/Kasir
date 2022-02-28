d6447f2bbcf0d6719503fba56d654ea91998ee92
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 01:33:23 +0800

    add check image path

--- app/Helpers/Globals.php
+++ app/Helpers/Globals.php
@@ -4,4 +4,7 @@ class Globals {
     public static function rupiah($angka) {
         return "Rp " . number_format($angka,2,',','.');
     }
+    public static function check_image_path($path) {
+        return str_contains($path, 'https') ? $path : asset('storage/' . $path);
+    }
 }
