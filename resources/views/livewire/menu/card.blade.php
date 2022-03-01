0bd07e692645b6ca9dbe3b83bbc8b0581fb97958
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 09:47:06 +0800

    edit breakpoints and image height

--- resources/views/livewire/menu/card.blade.php
+++ resources/views/livewire/menu/card.blade.php
@@ -1,15 +1,17 @@
-<div class="col-md-4 col-sm-6 mb-3">
+<div class="col-md-3 col-sm-4 mb-3">
     <div class="card">
-        <img src="{{ App\Helpers\Globals::check_image_path($menu->gambar) }}" class="card-img-top" alt="gambar" height="200px">
+        <img src="{{ App\Helpers\Globals::check_image_path($menu->gambar) }}" class="card-img-top" alt="gambar" height="100px">
         <div class="card-body">
-            <h5 class="card-title">{{ $menu->nama }}</h5>
-            <p>
+            <h6 class="card-title">{{ $menu->nama }}</h6>
+            <small>
                 <span>Harga : {{ App\Helpers\Globals::rupiah($menu->harga) }}</span><br>
                 <span>Stok : {{ $menu->stok }}</span><br>
                 {{ $menu->deskripsi }}
-            </p>
-            <a href="{{ route('menu.edit',$menu->id) }}" class="btn btn-primary">Edit</a>
-            <a wire:click="delete({{ $menu->id }})" class="btn btn-danger">Delete</a>
+            </small>
+            <div class="">
+                <a href="{{ route('menu.edit',$menu->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                <a wire:click="delete({{ $menu->id }})" class="btn btn-sm btn-danger">Delete</a>
+            </div>
         </div>
     </div>
 </div>
