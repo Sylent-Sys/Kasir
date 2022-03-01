95e4b9f4ecf79e7708d57d54dfa9622bca800a25
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 13:11:31 +0800

    new migrations

---
+++ database/migrations/2022_03_01_050857_create_transaksi_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('transaksi_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_transaksi_detail')->constrained('transaksi_details');
+            $table->foreignId('id_menu')->constrained('menus');
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi_items');
+    }
+}
---
+++ database/migrations/2022_03_01_050858_create_pembayarans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_transaksi_detail')->constrained('transaksi_details');
+            $table->integer('total');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+}
