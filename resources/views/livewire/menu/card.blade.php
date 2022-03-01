9f94c82cfaa066db6a882cbf0fd00a2af1c4070e
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 09:48:28 +0800

    edit again

--- resources/views/livewire/menu/card.blade.php
+++ resources/views/livewire/menu/card.blade.php
@@ -1,6 +1,7 @@
 <div class="col-md-3 col-sm-4 mb-3">
     <div class="card">
-        <img src="{{ App\Helpers\Globals::check_image_path($menu->gambar) }}" class="card-img-top" alt="gambar" height="100px">
+        <img src="{{ App\Helpers\Globals::check_image_path($menu->gambar) }}" class="card-img-top" alt="gambar"
+            height="100px">
         <div class="card-body">
             <h6 class="card-title">{{ $menu->nama }}</h6>
             <small>
@@ -8,10 +9,12 @@
                 <span>Stok : {{ $menu->stok }}</span><br>
                 {{ $menu->deskripsi }}
             </small>
-            <div class="">
-                <a href="{{ route('menu.edit',$menu->id) }}" class="btn btn-sm btn-primary">Edit</a>
-                <a wire:click="delete({{ $menu->id }})" class="btn btn-sm btn-danger">Delete</a>
-            </div>
+            @if (Request::routeIs('menu.index'))
+                <div>
+                    <a href="{{ route('menu.edit', $menu->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                    <a wire:click="delete({{ $menu->id }})" class="btn btn-sm btn-danger">Delete</a>
+                </div>
+            @endif
         </div>
     </div>
 </div>
