1720650897278010b04db13da8ea143a4b153844
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 11:30:43 +0800

    change how to looping data

--- resources/views/livewire/menu/index-menu.blade.php
+++ resources/views/livewire/menu/index-menu.blade.php
@@ -4,6 +4,8 @@
         <a href="{{ route('menu.add') }}" class="btn btn-primary"><i class="bi bi-plus-lg"></i></a>
     </div>
     <div class="row">
-        @each('livewire.menu.card', $data, 'menu')
+        @foreach ($data as $item)
+            @include('livewire.menu.card',['menu'=>$item,'options'=>['menu'=>true]])
+        @endforeach
     </div>
 </div>
