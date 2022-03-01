f32c721aca2f6181421cedd862a6e507a223b93c
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-01 11:30:05 +0800

    make changes

--- resources/views/livewire/menu/card.blade.php
+++ resources/views/livewire/menu/card.blade.php
@@ -6,11 +6,16 @@
             <h6 class="card-title">{{ $menu->nama }}</h6>
             <small class="d-block">Harga : {{ App\Helpers\Globals::rupiah($menu->harga) }}</small>
             <small class="d-block">Stok : {{ $menu->stok }}</small>
-            @if (Request::routeIs('menu.index'))
+            @if ($options['menu']??'')
                 <div>
                     <a href="{{ route('menu.edit', $menu->id) }}" class="btn btn-sm btn-primary">Edit</a>
                     <a wire:click="delete({{ $menu->id }})" class="btn btn-sm btn-danger">Delete</a>
                 </div>
+            @endif
+            @if ($options['pesanan']??'')
+            <div>
+                <input type="number" class="form-control" wire:model='data.{{ $menu->id }}.jumlah' min="0">
+            </div>
             @endif
         </div>
     </div>
