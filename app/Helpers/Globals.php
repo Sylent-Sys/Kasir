40c527d205857e9d3246b43bd20cc62cb221ca01
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-02 08:12:23 +0800

    :)

--- app/Helpers/Globals.php
+++ app/Helpers/Globals.php
@@ -2,7 +2,7 @@
 namespace App\Helpers;
 class Globals {
     public static function rupiah($angka) {
-        return "Rp " . number_format($angka,2,',','.');
+        return "Rp " . number_format($angka,2,',','.') . '-';
     }
     public static function check_image_path($path) {
         return str_contains($path, 'https') ? $path : asset('storage/' . $path);
