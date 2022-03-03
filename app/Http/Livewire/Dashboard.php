4ee3d792cc7331e97502b75608a76e49232fa222
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-03 16:34:24 +0800

    nambah event toastr

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Helpers\RoleUser;
 use App\Models\User;
 use Livewire\Component;
 
@@ -13,17 +14,22 @@ class Dashboard extends Component
     }
     public function activate(User $user) {
         $user->update(['is_aktif'=>true]);
+        $this->dispatchBrowserEvent('alert', ['type'=>'success','message'=>'Akun berhasil diaktifkan']);
     }
     public function deactivate(User $user) {
         $user->update(['is_aktif'=>false]);
+        $this->dispatchBrowserEvent('alert', ['type'=>'success','message'=>'Akun berhasil dinonaktifkan']);
     }
     public function promote(User $user) {
         $user->update(['role'=>$user->role+1]);
+        $this->dispatchBrowserEvent('alert', ['type'=>'success','message'=>'Akun berhasil dipromosikan menjadi '.RoleUser::getLabel($user->role)]);
     }
     public function demote(User $user) {
         $user->update(['role'=>$user->role-1]);
+        $this->dispatchBrowserEvent('alert', ['type'=>'success','message'=>'Akun berhasil didemosikan menjadi '.RoleUser::getLabel($user->role)]);
     }
     public function delete(User $user) {
         $user->delete();
+        $this->dispatchBrowserEvent('alert', ['type'=>'success','message'=>'Akun berhasil dihapus']);
     }
 }
