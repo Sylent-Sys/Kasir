bf9409d877949d0f3c9036165c6c196180acde6f
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-03 19:44:23 +0800

    semua selesai

--- app/Http/Livewire/Transaksi/FormPembayaran.php
+++ app/Http/Livewire/Transaksi/FormPembayaran.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire\Transaksi;
 
+use App\Models\Pembayaran;
+use App\Models\TransaksiDetail;
 use Livewire\Component;
 
 class FormPembayaran extends Component
 {
+    public TransaksiDetail $transaksiDetail;
+    public $membayar = 0;
     public function render()
     {
         return view('livewire.transaksi.form-pembayaran');
+    }
+    public function pembayaran($kembalian)
+    {
+        if (Pembayaran::create([
+            'id_user'=>auth()->user()->id,
+            'id_transaksi_detail' => $this->transaksiDetail->id,
+            'bayar'=>$this->membayar,
+            'kembalian'=>$kembalian
+        ])->save()) {
+            return redirect(route('transaksi.index'));
+        }
     }
 }
--- resources/views/livewire/transaksi/form-pembayaran.blade.php
+++ resources/views/livewire/transaksi/form-pembayaran.blade.php
@@ -1,3 +1,62 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    @include('layouts.title', [
+        'title' => 'Form Pembayaran',
+    ])
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Menu</th>
+                    <th>Jumlah</th>
+                    <th>Harga</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transaksiDetail->transaksiItems()->get() as $item)
+                    <tr>
+                        <td>{{ $loop->index + 1 }}</td>
+                        <td>{{ $item->menu->nama }}</td>
+                        <td>{{ $item->jumlah }}</td>
+                        <td>{{ $item->menu->harga }}</td>
+                        <td>{{ App\Helpers\Globals::rupiah($item->jumlah * $item->menu->harga) }}</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="4" class='text-center fw-bold'>Total</td>
+                    <td>{{ App\Helpers\Globals::rupiah($transaksiDetail->total) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="4" class='text-center fw-bold'>No Meja</td>
+                    <td>{{ $transaksiDetail->no_meja }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">Membayar : </label>
+        <div class="col-sm-10">
+            <input class="form-control" type="integer" wire:model.lazy='membayar'>
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">Kembalian : </label>
+        <div class="col-sm-10">
+            @php
+                if ($membayar == '') {
+                    $membayar = 0;
+                }
+                $kembalian = $membayar - $transaksiDetail->total;
+                if($kembalian < 0) {
+                    $kembalian = 0;
+                    $this->dispatchBrowserEvent('alert', ['type'=>'error','message'=>'Uang pembayaran kurang']);
+                }
+            @endphp
+            <input class="form-control" type="integer" value="{{ $kembalian }}" readonly>
+        </div>
+    </div>
+    <div class="mb-3">
+        <button class="btn btn-primary" wire:click='pembayaran({{ $kembalian }})'><i class="bi bi-credit-card"></i></button>
+    </div>
 </div>
