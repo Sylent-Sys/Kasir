46e3b6a9c9fb04fad77a7733e0395cbbbfe06045
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-03 13:02:13 +0800

    pelaporan selesai

--- app/Http/Livewire/Laporan/IndexLaporan.php
+++ app/Http/Livewire/Laporan/IndexLaporan.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire\Laporan;
 
+use App\Models\Menu;
 use Livewire\Component;
 
 class IndexLaporan extends Component
 {
     public function render()
     {
-        return view('livewire.laporan.index-laporan');
+        return view('livewire.laporan.index-laporan',['menu'=>Menu::with('transaksiItems')->get()]);
     }
 }
--- resources/views/livewire/laporan/index-laporan.blade.php
+++ resources/views/livewire/laporan/index-laporan.blade.php
@@ -1,3 +1,43 @@
 <div>
-    {{-- Be like water. --}}
+    @include('layouts.title',['title'=>'Laporan'])
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nama</th>
+                    <th>Sisa Stok</th>
+                    <th>Jumlah Terjual</th>
+                    <th>Harga</th>
+                    <th>Total Pemasukan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $totalPemasukan = 0;
+                @endphp
+                @forelse ($menu as $item)
+                    <tr>
+                        <td>{{ $loop->index + 1 }}</td>
+                        <td>{{ $item->nama }}</td>
+                        <td>{{ $item->stok }}</td>
+                        <td>{{ $item->transaksiItems->sum('jumlah') }}</td>
+                        <td>{{ \App\Helpers\Globals::rupiah($item->harga) }}</td>
+                        @php
+                            $totalPemasukan += $item->transaksiItems->sum('jumlah') * $item->harga;
+                        @endphp
+                        <td>{{ \App\Helpers\Globals::rupiah($item->transaksiItems->sum('jumlah') * $item->harga) }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-center">Tidak ada data</td>
+                    </tr>
+                @endforelse
+                <tr>
+                    <td colspan="5">Total Pemasukan</td>
+                    <td>{{ \App\Helpers\Globals::rupiah($totalPemasukan) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
