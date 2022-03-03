a7453a39315552aae38cc07b69e49681d6615e07
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-03 16:34:34 +0800

    rubah urutan role

--- app/Helpers/RoleUser.php
+++ app/Helpers/RoleUser.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Helpers;
 class RoleUser {
-    public const ADMIN = 1;
-    public const PENGGUNA = 2;
-    public const WAITER = 3;
-    public const KASIR = 4;
-    public const OWNER = 5;
+    public const PENGGUNA = 1;
+    public const WAITER = 2;
+    public const KASIR = 3;
+    public const OWNER = 4;
+    public const ADMIN = 5;
     public static function getLabel($role) {
         return match($role) {
             RoleUser::ADMIN => 'Admin',
