58b7a5ef54c8c4dfbe7e6e81178ce9b6414ab6db
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-04 13:35:58 +0800

    add more role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,12 +16,40 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
+            'name' => 'pengguna',
+            'role'=>RoleUser::PENGGUNA,
+            'email' => 'pengguna@pengguna.com',
+            'password' => bcrypt('pengguna'),
+            'is_aktif'=>true
+        ]);
+        User::create([
+            'name' => 'waiter',
+            'role'=>RoleUser::WAITER,
+            'email' => 'waiter@waiter.com',
+            'password' => bcrypt('waiter'),
+            'is_aktif'=>true
+        ]);
+        User::create([
+            'name' => 'kasir',
+            'role'=>RoleUser::KASIR,
+            'email' => 'kasir@kasir.com',
+            'password' => bcrypt('kasir'),
+            'is_aktif'=>true
+        ]);
+        User::create([
+            'name' => 'owner',
+            'role'=>RoleUser::OWNER,
+            'email' => 'owner@owner.com',
+            'password' => bcrypt('owner'),
+            'is_aktif'=>true
+        ]);
+        User::create([
             'name' => 'admin',
             'role'=>RoleUser::ADMIN,
             'email' => 'admin@admin.com',
             'password' => bcrypt('admin'),
             'is_aktif'=>true
         ]);
-        User::factory()->count(5)->create();
+        // User::factory()->count(5)->create();
     }
 }
