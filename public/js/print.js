2df3d0db50ae2991933afd8a8c1e6bd566fefb6b
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-04 21:10:43 +0800

    bug fix

--- public/js/print.js
+++ public/js/print.js
@@ -1,14 +1,12 @@
 function printDiv(divName) {
+    $("#remove").hide();
     var printContents = document.getElementById(divName).innerHTML;
     var originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
+    $("#remove").show();
 }
 document.getElementById('ct').onclick = function() {
-    $("#remove").remove();
     printDiv('printArea');
 }
-$(document).ready(function() {
-    $("remove").remove();
-});
