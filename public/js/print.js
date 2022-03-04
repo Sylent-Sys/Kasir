28e012d25855e7ad6a24a843361e761f95256a6d
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-03-04 14:20:23 +0800

    make print.js

---
+++ public/js/print.js
@@ -0,0 +1,14 @@
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
+document.getElementById('ct').onclick = function() {
+    $("#remove").remove();
+    printDiv('printArea');
+}
+$(document).ready(function() {
+    $("remove").remove();
+});
