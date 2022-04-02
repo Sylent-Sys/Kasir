1be20aa9fed37cab3113b15e724784930943472c
Renaldi A.K <renaldiapriyanto419@gmail.com>
2022-04-02 12:51:21 +0800

    trim useless propery

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,6 @@
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Kasir</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu">
         <span class="navbar-toggler-icon"></span>
     </button>
     <input class="form-control form-control-dark bg-dark w-100 " type="text" disabled>
